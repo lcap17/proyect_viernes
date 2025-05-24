4086894d3c73c952d85c9a3febf8bae5bb185de7
lcap17 <lcap1726@gmail.com>
2025-05-23 19:18:17 -0500

    c2

--- Inicio.py
+++ Inicio.py
@@ -130,7 +130,7 @@ with col2:
     st.markdown('<h3 style="color: #0066cc; margin-top: 0px;">Laura Catalina Acevedo Posada</h3>', unsafe_allow_html=True)
     st.markdown('<p style="margin-top: 10px;">Programa: <span style="color: #0066cc; font-weight: bold;">Desarrollo de Software</span></p>', unsafe_allow_html=True)
     st.markdown('<p>Semestre: <span style="color: #0066cc; font-weight: bold;">2025-1</span></p>', unsafe_allow_html=True)
-    st.markdown('<p>Repositorio: <a href="https://github.com/username/proyecto-ntp" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
+    st.markdown('<p>Repositorio: <a href="https://github.com/lcap17/proyect_viernes.git" target="_blank" style="color: #0066cc; font-weight: bold; text-decoration: none;">GitHub</a></p>', unsafe_allow_html=True)
    
 
 
